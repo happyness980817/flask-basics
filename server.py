1f1ad5d68669649b02ba85b49c2a282d4b4c7f5f
happyness980817 <happyness980817@gmail.com>
2024-06-22 14:01:09 +0900

    why flask?

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hi'
+    return 'random : <strong>' +str(random.random())+'</strong>' # 브라우저에 html 전달
 
 app.run(debug=True) #서버 닫지 않아도 새로고침하면 변경사항 반영, 실제 서비스시에는 X
 
