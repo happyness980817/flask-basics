3d87cdc1ae32c65564cea4b422c7dc3dcbd15b83
happyness980817 <happyness980817@gmail.com>
2024-06-23 20:37:18 +0900

    Create (2)

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
+nextId = 4
 topics = [
     {'id':1, 'title': 'html', 'body': 'html is ...'},
     {'id':2, 'title': 'css', 'body': 'css is ...'},
@@ -35,16 +36,27 @@ def getContents():
 def index():
     return template(getContents(), '<h2>Welcome</h2>Hello, WEB')
 
-@app.route('/create/')
+@app.route('/create/', methods=['GET','POST']) # get 이 아닌 method 허용할 시 methods 지정
 def create():
-    content = '''
-        <form action="/create/" method="POST"> 
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p><textarea name="body" placeholder="body"></textarea></p>  
-            <p><input type="submit" value="create"></p>
-        </form>
-    '''
-    # POST 방식 - 데이터가 url 을 통해 전송되지 않음. 데이터를 사용자가 변경할 때 사용
+    if request.method == 'GET':
+        content = '''
+            <form action="/create/" method="POST"> 
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p><textarea name="body" placeholder="body"></textarea></p>  
+                <p><input type="submit" value="create"></p>
+            </form>
+        '''
+    elif request.method == 'POST':
+        global nextId # 외부에서 생성된 전역변수 초기값 참조
+        title = request.form['title']
+        body = request.form['body']
+        newTopic = {'id':nextId, 'title': title, 'body': body}
+        topics.append(newTopic)
+        url = '/read/' + str(nextId) + '/'
+        nextId += 1
+        return redirect(url) # redirection: redirects a user to another endpoint
+
+    # POST 방식(method) - 데이터가 url 을 통해 전송되지 않음. 데이터를 사용자가 변경할 때 사용
     return template(getContents(),content)
 
 @app.route('/read/<int:id>/') # 읽기, id 정수로 지정
